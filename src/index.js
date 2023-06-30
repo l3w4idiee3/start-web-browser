6da38c6e970307df8d2bb49a9f4ca75994b7ec03
l3w4idiee3 <lewisjassy43@gmail.com>
2023-06-30 21:45:14 +0300

    creating some const variables to hold the values associated with various fields

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 //1
 // form fields
+const form = document.querySelector('.form-data');
+const region = document.querySelector('.regeion-name')
+const apiKey = document.querySelector('.api-key');
 // results divs
-
+const erros = document.querySelector('.errors');
+const loading = document.querySelector('.loading');
+const results = document.querySelector('.result-container');
+const usage = document.querySelector('.carbon usage');
+const fossilfuel = document.querySelector('.fossil-fuel');
+const myregion = document.querySelector('.myregion');
+const clearBtn = document.querySelector('.clear-btn');
 //6
 //call the API
 
