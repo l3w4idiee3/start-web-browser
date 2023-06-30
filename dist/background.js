a705dd30ef5ee563bbfb48d16034923c5b2ce025
l3w4idiee3 <lewisjassy43@gmail.com>
2023-06-30 21:47:29 +0300

    added an event listener

--- dist/background.js
+++ dist/background.js
@@ -1,4 +1,6 @@
 //add listener here
-
+form.addEventListener('submit', (e) => handleSubmit(e));
+clearBtn.addEventListener('click', (e) => reset(e));
+init();
 //borrowed from energy lollipop extension
 //draw the icon here
