17a70742faeca5068c36db982b23850d16a9a48c
l3w4idiee3 <lewisjassy43@gmail.com>
2023-07-01 05:13:34 +0300

    calculate color

--- src/index.js
+++ src/index.js
@@ -11,6 +11,22 @@ const usage = document.querySelector('.carbon usage');
 const fossilfuel = document.querySelector('.fossil-fuel');
 const myregion = document.querySelector('.myregion');
 const clearBtn = document.querySelector('.clear-btn');
+function calculateColor(value) {
+    let co2Scale = [0, 150, 600, 750, 800];
+    let colors = ['#2AA364', '#F5EB4D', '#9E4229', '#381D02', '#381D02'];
+
+    let closestNum = co2Scale.sort((a, b) => {
+        return Math.abs(a - value) - Math.abs(b - value);
+    })[0];
+    console.log(value + ' is closest to ' + closestNum);
+    let num = (element) => element > closestNum;
+    let scaleIndex = co2Scale.findIndex(num);
+
+    let closestColor = colors[scaleIndex];
+    console.log(scaleIndex, closestColor);
+
+    chrome.runtime.sendMessage({ action: 'updateIcon', value: { color: closestColor } });
+}
 //6
 //call the API
 import axios from '../node_modules/axios';
